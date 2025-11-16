e51b3394f0b60b7fa98f91f52f2e0c846feba0aa
Aaron Yeh <137107162+marvelcn015@users.noreply.github.com>
2025-11-17 01:09:50 +0800

    feat: integrate OpenAPI Generator

---
+++ src/frontend/lib/api-client/apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './DashboardApi';
+export * from './DealManagementApi';
+export * from './HealthApi';
+export * from './KPIManagementApi';
+export * from './ParametersApi';
+export * from './SettlementApi';
+export * from './TimelineApi';
+export * from './WalrusApi';
